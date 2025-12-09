5d6d73939b9eeab50303f50436790f77f91eccba
Masta-isshakh <mastaisshakh@gmail.com>
2025-12-09 04:25:35 -0800

    home et contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Link } from 'react-router-dom';
+import HomePage from "./Homepage";
+import {Routes, Route} from 'react-router';
+import Contact from './Contact'
 
 const client = generateClient<Schema>();
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -18,22 +23,41 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <div>
+            {/* Header */}
+      <header className={menuOpen ? "open" : ""}>
+        <div className="logo">
+          <img
+            src="/src/assets/logo.jpeg"
+            alt="Rodeo Drive Logo"
+            className="logo-img"
+          />
+          <span>Rodeo Drive</span>
+        </div>
+
+        {/* Mobile Toggle */}
+        <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Navigation */}
+        <nav> 
+          <Link to="/">Home</Link> 
+          <Link to="/Services">Services</Link> 
+          <Link to="/About">About</Link> 
+          <Link to="/Contact">Contact</Link> 
+          {/* <-- redirection vers page Contact */} 
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/services" element={<h1>Services Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </div>
   );
 }
 
