a2a0f695ec478b03b1df85a07f39c6a955684078
Masta-isshakh <mastaisshakh@gmail.com>
2025-12-10 08:48:16 -0800

    avec traduction

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./i18n.js";
+import "./auth-theme.css";
+import '@aws-amplify/ui-react/styles.css';
 
 
 import "./index.css";
